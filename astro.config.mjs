f7551c6d521a0209d75f32885a6abafddff26045
lihawhaw <lihawhaw@163.com>
2023-04-25 23:46:41 +0800

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import prefetch from '@astrojs/prefetch'
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import { EnumChangefreq } from 'sitemap';
 
+const lastmod = new Date().toString()
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://liyizhuang.com/',
+  site: 'https://liyizhuang.com',
   build: {
     assets: 'static',
     // assetsPrefix: 'https://static.lihaha.cn'
@@ -14,5 +17,26 @@ export default defineConfig({
     port: 6001,
     host: true,
   },
-  integrations: [tailwind(), prefetch(), mdx()],
+  integrations: [
+    tailwind(),
+    prefetch(),
+    mdx(),
+    sitemap({
+      changefreq: EnumChangefreq.WEEKLY,
+      priority: 1,
+      serialize(item) {
+        if (/work/.test(item.url)) {
+          item.changefreq = EnumChangefreq.WEEKLY
+          item.lastmod = lastmod
+          item.priority = 0.7
+        }
+        if (/post/.test(item.url)) {
+          item.changefreq = EnumChangefreq.DAILY
+          item.lastmod = lastmod
+          item.priority = 0.8
+        }
+        return item
+      },
+    }),
+  ],
 })
